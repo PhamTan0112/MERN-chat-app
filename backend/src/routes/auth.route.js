d3e46c94eec597057b1fcb3ca5d3249df246d078
Taann <phamphutanxo@gmail.com>
2025-08-26 23:42:23 +0700

    feat: create auth Login-logout-updateProfile

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { login, logout, signup } from "../controller/auth.controller.js";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -7,5 +15,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
